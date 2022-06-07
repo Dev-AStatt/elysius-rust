f24fc47cbc475a0860b6e0e7d1e32754e2b1f96d
AStatt-Desktop <AStatt@Protonmail.com>
2022-06-06 19:50:32 -0700

    Started migrating project to ECS model

--- src/elysius_game_loop.rs
+++ src/elysius_game_loop.rs
@@ -1,10 +1,41 @@
 use crate::olc_pixel_game_engine as olc;
 
 
-mod solar_objects;
+//mod solar_objects;
+
+
+// 0------------Start of ECS System---------------0
+type EntityIndex = usize;
+
+
+struct OrbitComponent {
+    radius: i32,
+    angle: i32,
+}
+struct DrawingComponents {
+    circle_size: i32,
+    color: (u8,u8,u8),
+}
+
+
+struct Entity {
+    orbit: Option<OrbitComponent>,
+    draw_info: Option<DrawingComponents>,
+    solar_pos: (i32,i32),
+}
+
+// 0------------End of ECS System---------------0
+
 
 pub struct ElysiusProgram {
-    system_1: solar_objects::SolarSystem,
+    //Old stuff for OOP Solar System
+    //system_1: solar_objects::SolarSystem,
+
+    //ECS
+    entities: Vec<Option<Entity>>,
+    entities_ID: Vec<EntityIndex>,
+
+
     tick_update: bool,
     accumulated_time: f32,
     game_tick: i32,
@@ -16,6 +47,23 @@ impl olc::Application for ElysiusProgram {
     fn on_user_create(&mut self) -> Result<(), olc::Error> {
         // Mirrors `olcPixelGameEngine::onUserCreate`. Your code goes here.
         
+
+        //make our fist ECS thing
+        let new_ent = Entity {
+            orbit: Some(OrbitComponent {
+                radius: 5,
+                 angle: 0 }),
+            draw_info: Some(DrawingComponents {
+                circle_size: 5, 
+                color: (255,255,0)}),
+            solar_pos: (100,50),
+        };
+        self.entities.push(Some(new_ent));
+        self.entities_ID.push(0);
+
+        //end of ecs thing
+
+
         Ok(())
     }
 
@@ -29,14 +77,18 @@ impl olc::Application for ElysiusProgram {
 
         if self.tick_update {
             //Use this to update the solar system positions 
-            self.system_1.update_bodies();
+            //self.system_1.update_bodies();
         }
 
         //Draw Solar System
-        self.draw_solar_object_by_ref(&self.system_1.sun);
-        for n in 0..self.system_1.numb_of_bodies() {
-            self.draw_solar_object_by_ref(&self.system_1.orbiting_bodies[n as usize])
-        }
+        // self.draw_solar_object_by_ref(&self.system_1.sun);
+        // for n in 0..self.system_1.numb_of_bodies() {
+        //     self.draw_solar_object_by_ref(&self.system_1.orbiting_bodies[n as usize])
+        // }
+
+        //Draw ECS Ent
+        self.draw_solar_object_ecs(0);
+
         
         
 
@@ -55,10 +107,12 @@ impl olc::Application for ElysiusProgram {
 impl ElysiusProgram {
     pub fn new() -> Self {
         ElysiusProgram {
-            system_1: solar_objects::SolarSystem::new((5,5)),
+            //system_1: solar_objects::SolarSystem::new((5,5)),
             tick_update: false,
             accumulated_time: 0.0,
             game_tick: 0,
+            entities: Vec::new(),
+            entities_ID: Vec::new(),
         }
     }
 
@@ -79,11 +133,30 @@ impl ElysiusProgram {
 //  0---------------------------0
 //  | Drawing Functions         |
 //  0---------------------------0
-    //function takes in the solar object to draw and then prints it into olc
-    fn draw_solar_object_by_ref(self: &Self, obj: &solar_objects::SolarObject) {
-        //create a olc::Pixel from the rgb touple stored in solar object
-        let col = olc::Pixel::rgb(obj.color.0, obj.color.1, obj.color.2);  
-        olc::fill_circle(obj.solar_pos.0, obj.solar_pos.1, obj.size, col);
+    fn draw_solar_object_ecs(self: &Self, ent_id: usize) {
+        match self.entities[ent_id]  {
+            None => return,
+            Some(ref ent) => {
+                match ent.draw_info {
+                    None => return,
+                    Some(ref draw_ent) => {
+                        let col = olc::Pixel::rgb(draw_ent.color.0, draw_ent.color.1,draw_ent.color.2);
+                        olc::fill_circle(ent.solar_pos.0, ent.solar_pos.1, draw_ent.circle_size, col)
+                    }
+                }
+            }
+        }  
     }
+        //let col = olc::Pixel::rgb(self.entities[ent_id].);  
 }
+
+
+
+    //function takes in the solar object to draw and then prints it into olc
+    // fn draw_solar_object_by_ref(self: &Self, obj: &solar_objects::SolarObject) {
+    //     //create a olc::Pixel from the rgb touple stored in solar object
+    //     let col = olc::Pixel::rgb(obj.color.0, obj.color.1, obj.color.2);  
+    //     olc::fill_circle(obj.solar_pos.0, obj.solar_pos.1, obj.size, col);
+    // }
+
 
