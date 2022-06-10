59303b1f66c9ac8e3ffafa36d279b86af286496a
AStatt-Desktop <AStatt@Protonmail.com>
2022-06-10 09:28:36 -0700

    Drawing FPS and Current Tick to the screen

--- src/main.rs
+++ src/main.rs
@@ -13,46 +13,74 @@ use std::{env, path};
 const SCREEN_SIZE: (f32, f32) = (1024 as f32,1024 as f32);
 
 
-struct sprite_info {
+
+// 0------------------Start of ECS Sstem---------------------------------------0
+type EntityIndex = usize;
+
+struct OrbitalComponent {
+    radius: i32,
+    angle: f32,
+}
+
+struct DrawingComponent {
     sprite: graphics::Image,
     image_size: (i32, i32),
+    
 }
+
+struct Entity {
+    orbit: Option<OrbitalComponent>,
+    draw_info: DrawingComponent,
+    solar_pos: (i32, i32),
+    solar_system_id: i32,
+}
+
+
+// 0--------------------End of ECS Sstem---------------------------------------0
+
 
 //
 //  To add a sprite, add a new item into MainState Struct pointing to graphics::Image
 //  Then load the texture in the new() impl of MainState. Call it with the draw function. 
 //
 
-struct MainState {
-    pos_x: f32,
-    sun_image: graphics::Image,
-    sun_image_1: sprite_info,
-    
+//MAIN GAME STRUCT
+struct ElysiusMainState {
+    test_sun_image: DrawingComponent,
+    //ECS
+    entities: Vec<Option<Entity>>,
+    entities_id: Vec<EntityIndex>,
+ 
 }
 
-impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
+
+impl ElysiusMainState {
+    fn new(ctx: &mut Context) -> GameResult<ElysiusMainState> {
 
         //Loading Sprites
-        
-        let sun_image = graphics::Image::from_path(ctx, "/Sprite-SUN_01.png", true)?;
-        let sun_image_1 = sprite_info {
+        let test_sun_image = DrawingComponent {
             sprite: graphics::Image::from_path(ctx, "/Sprite-SUN_01.png", true)?,
             image_size: (128,128) };
 
     
 
-        Ok(MainState {
-            sun_image,
-            sun_image_1,
-            pos_x: 0.0,
+        Ok(ElysiusMainState {
+            test_sun_image, 
+            entities: Vec::new(),
+            entities_id: Vec::new(),
             })
     }
+
+  
+
 }
 
-impl event::EventHandler<ggez::GameError> for MainState {
+impl event::EventHandler<ggez::GameError> for ElysiusMainState {
+    //Update events go in this function
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+        
+        
+
         Ok(())
     }
 
@@ -62,29 +90,42 @@ impl event::EventHandler<ggez::GameError> for MainState {
             graphics::CanvasLoadOp::Clear([0.2, 0.2, 0.2, 1.0].into()),
         );
 
-        //Drawing Sprite
-        let dst = glam::Vec2::new(20.0, 20.0);
-        let scale = glam::Vec2::new(0.5, 0.5);
-        canvas.draw(&self.sun_image,
-            graphics::DrawParam::new()
-                .dest(dst)
-                .scale(scale)
-            );
-
+        //Calculate Position and Scale
         let dst = glam::Vec2::new(400.0, 400.0);
         let scale = glam::Vec2::new(1.0, 1.0);
-        canvas.draw(&self.sun_image_1.sprite,
+        //Draw Sprite
+        canvas.draw(&self.test_sun_image.sprite,
             graphics::DrawParam::new()
                 .dest(dst)
                 .scale(scale)
             );
+        
+        //Concatinating strings is dumb
+        let mut str = String::from("Tick: ");
+        str.push_str(&ctx.time.ticks().to_string());
+        //Draw the current tick to the screen
+        canvas.draw(graphics::Text::new(str)
+                    .set_scale(10.0),
+                    glam::Vec2::new(0.0,0.0));
 
 
+        //Draw the FPS counter
+        ctx.gfx.set_window_title(&format!(
+            "Elysius - {:.0} FPS", ctx.time.fps()));
+        
+
+        //Nothing after this, pushes all the draws to the graphics card
         canvas.finish(ctx)?;
-
         Ok(())
     }
 }
+
+
+
+
+
+
+// 0---------------------PROGRAM MAIN------------------------------------------0
 
 pub fn main() -> GameResult {
     //added in to add resources dir
@@ -96,8 +137,6 @@ pub fn main() -> GameResult {
         path::PathBuf::from("./resources")
     };
     //End of Resource Directory Stuff
-
-
     let cb = ggez::ContextBuilder::new("super_simple", "ggez")
         //Tell context builder where to find the resources for our game
         .add_resource_path(resource_dir)
@@ -109,6 +148,6 @@ pub fn main() -> GameResult {
     // And finally we attempt to build the context and create the window. If it fails, we panic with the message
     // "Failed to build ggez context"
     let (mut ctx, event_loop) = cb.build()?;
-    let state = MainState::new(&mut ctx)?;
+    let state = ElysiusMainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
 }
