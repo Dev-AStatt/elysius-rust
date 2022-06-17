37e87cbde84f70423159427b9faee0247d5197f9
AStatt-Desktop <AStatt@Protonmail.com>
2022-06-17 16:19:08 -0700

    added a sub menu struct

--- src/menus.rs
+++ src/menus.rs
@@ -6,21 +6,39 @@ use ggez::{
 use glam::{f32, i32, vec2};
 use crate::globs;
 
+struct MenuBodyPositions {
+    pos: glam::Vec2,
+    name_pos: glam::Vec2,
+    resources_pos: glam::Vec2,
+    sprite_pos: glam::Vec2,
+}
+    impl MenuBodyPositions {
+        pub fn new() -> Self {
+            let pos = glam::Vec2::new(30.0,30.0);
+            let name_pos = glam::Vec2::new(pos.x + 40.0, pos.y + 15.0);
+            let resources_pos = glam::Vec2::new(0.0,0.0);
+            let sprite_pos = glam::Vec2::new(0.0,0.0);
+
+            MenuBodyPositions {
+                pos,
+                name_pos,
+                resources_pos,
+                sprite_pos,
+            }
+        }
+    }
+
 pub struct Menus {
     pub body_texture: graphics::Image,
-    menu_pos: glam::Vec2,
-    menu_name_pos: glam::Vec2,
+    menu_body_pos: MenuBodyPositions,
 }
 
 impl Menus {
     pub fn new(texture: graphics::Image) -> Self {
-        let menu_pos = glam::Vec2::new(30.0,30.0);
-        let menu_name_pos = glam::Vec2::new(menu_pos.x + 40.0, menu_pos.y + 15.0);
-
+        
         Menus {
             body_texture: texture,
-            menu_pos,
-            menu_name_pos, 
+            menu_body_pos: MenuBodyPositions::new(),
         }
     }
 
@@ -32,12 +50,12 @@ impl Menus {
         ent_id: usize,
     ) {
         //Draw Menu
-        canvas.draw(&self.body_texture, self.menu_pos);
+        canvas.draw(&self.body_texture, self.menu_body_pos.pos);
         //Draw Name
         let str = &ents.ent_name[ent_id];
         canvas.draw(graphics::Text::new(str)
                     .set_scale(20.0),
-                    self.menu_name_pos);
+                    self.menu_body_pos.name_pos);
 
     }
 
