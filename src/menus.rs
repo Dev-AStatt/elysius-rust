4266632234883b4603cce8d1712a49b1147c1d78
AStatt-Desktop <AStatt@Protonmail.com>
2022-06-18 14:29:56 -0700

    Menu Updates

--- src/menus.rs
+++ src/menus.rs
@@ -9,8 +9,8 @@ use ggez::{
 struct MenuBodyPositions {
     pos: glam::Vec2,
     name_pos: glam::Vec2,
-    pos_coal_sprite: glam::Vec2,
-    pos_energy_sprite: glam::Vec2,
+    pos_coal_text: glam::Vec2,
+    pos_radioactive_text: glam::Vec2,
 }
     impl MenuBodyPositions {
         pub fn new() -> Self {
@@ -18,18 +18,18 @@ struct MenuBodyPositions {
             let name_pos = glam::Vec2::new(
                 pos.x + 40.0,
                 pos.y + 15.0);
-            let pos_coal_sprite = glam::Vec2::new(
-                pos.x + 265.0,
-                pos.y + 64.0);
-            let pos_energy_sprite = glam::Vec2::new(
-                pos.x + 265.0,
-                pos.y + 140.0);
+            let pos_coal_text = glam::Vec2::new(
+                pos.x + 305.0,
+                pos.y + 70.0);
+            let pos_radioactive_text = glam::Vec2::new(
+                pos.x + 305.0,
+                pos.y + 112.0);
 
             MenuBodyPositions {
                 pos,
                 name_pos,
-                pos_coal_sprite,
-                pos_energy_sprite,
+                pos_coal_text,
+                pos_radioactive_text,
             }
         }
     }
@@ -42,8 +42,7 @@ impl Default for MenuBodyPositions {
 
 pub struct Menus {
     pub body_texture: graphics::Image,
-    coal_texture: graphics::Image,
-    energy_texture: graphics::Image,
+
     menu_body_pos: MenuBodyPositions,
     
 }
@@ -52,9 +51,8 @@ impl Menus {
     pub fn new(ctx: &Context) -> Self {
 
         Menus {
-            body_texture: ecs::sprite_get(ctx, "/menu_01.png"),
-            coal_texture: ecs::sprite_get(ctx, "/Sprite-Coal_01.png"),
-            energy_texture: ecs::sprite_get(ctx, "/Sprite-Energy_01.png"),
+            body_texture: ecs::sprite_get(ctx, "/menu_02.png"),
+
             menu_body_pos: MenuBodyPositions::new(),
         }
     }
@@ -69,19 +67,27 @@ impl Menus {
         canvas.draw(
             &self.body_texture,
             self.menu_body_pos.pos);
-        //Draw Sprites
-        canvas.draw(
-            &self.coal_texture,
-            self.menu_body_pos.pos_coal_sprite);
-        canvas.draw(
-            &self.energy_texture,
-            self.menu_body_pos.pos_energy_sprite);
+
         //Draw Name
         canvas.draw(
-            graphics::Text::new(&ents.ent_name[ent_id])
-                    .set_scale(20.0),
-            self.menu_body_pos.name_pos);
-        //Draw Icons
+            graphics::Text::new(
+                &ents.ent_name[ent_id]).set_scale(20.0),
+                self.menu_body_pos.name_pos);
+        //Draw Coal Amount
+        match &ents.energy_comp[ent_id] {
+            None => {},
+            Some(ref e_comp) => {
+                canvas.draw(
+                    graphics::Text::new(
+                        e_comp.fossil.to_string()).set_scale(25.0),
+                        self.menu_body_pos.pos_coal_text);
+                canvas.draw(
+                    graphics::Text::new(
+                        e_comp.radioactive.to_string()).set_scale(25.0),
+                        self.menu_body_pos.pos_radioactive_text);
+            },
+            
+        }
 
     }
 
