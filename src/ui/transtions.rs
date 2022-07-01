9dc73d7a8b75149de8efccaf4c9d46488244be96
AStatt-Desktop <AStatt@Protonmail.com>
2022-06-30 18:12:01 -0700

    Added a increment transition and arrived at dest function

--- src/ui/transtions.rs
+++ src/ui/transtions.rs
@@ -7,70 +7,71 @@ pub enum TransitionType {
 
 pub struct Transition {
     in_transition: bool,
-    transition_type: TransitionType,
-    pos_start: glam::Vec2,
+    t_type: TransitionType,
     pos_end: glam::Vec2,
     pos: glam::Vec2,
     dpt: glam::Vec2,
+    arrived: bool,
 
 }
 
 impl Transition {
     pub fn new(
-        transition_type: TransitionType,
+        t_type: TransitionType,
         pos_start: glam::Vec2,
         pos_end: glam::Vec2
     ) -> Self {
-        let mut t = Transition {
+        let dpt = Transition::get_dist_per_tick(&t_type, pos_start, pos_end, 60.0);
+        Transition {
             in_transition: true,
-            transition_type,
-            pos_start,
+            t_type,
             pos_end,
-            dpt: glam::Vec2::new(0.0,0.0),
             pos: pos_start,
-        };
-        t.get_dist_per_tick(2.0); 
-        return t;
+            dpt,
+            arrived: false,
+        }
     }
 
 
     pub fn is_in_transition(&self) -> bool {return self.in_transition;}
 
     pub fn inc_transition(self: &mut Self) {
-
+        if self.arived_at_dest() {
+            self.arrived = true;
+        } else {
+            //Incriment Position
+            self.pos += self.dpt;
+        }
     }
 
     fn arived_at_dest(&self) -> bool {
-        
-        //if position is within plus or minus dpt of the end position return true
-        //get Square
-        let area_p1 = self.pos_end + self.dpt;
-        let area_p2 = self.pos_end - self.dpt;
-        
+        let t_v = f32::min(self.dpt.x, self.dpt.y);
+        if (self.pos.x - self.pos_end.x).powi(2) + (self.pos.y - self.pos_end.y).powi(2) < t_v.powi(2) {
+            return true;
+        } else {return false;}
+    }   
 
 
-        return false;
-    }
-
-    fn point_in_square(s_1: glam::Vec2, s_2: glam::Vec2, p: glam::Vec2) -> bool {
-        return true;
-    }
-
-
-    fn get_dist_per_tick(self: &mut Self, ticks: f32) {
-
-        match self.transition_type {
-            TransitionType::Pop => {
-                self.dpt = glam::Vec2::new(0.0,0.0);
-            }
+    fn get_dist_per_tick(
+        t_t: &TransitionType,
+        pos: glam::Vec2, 
+        p_end: glam::Vec2, 
+        ticks: f32
+    ) -> glam::Vec2 {
+        let mut dpt = glam::Vec2::new(0.0,0.0);
+        match t_t {
+            TransitionType::Pop => {}
             TransitionType::Slide => {
                 //Get the total distance we need to move
-                let total = self.pos_end - self.pos_start; 
-                self.dpt = total.div(ticks);
+                let total = p_end - pos; 
+                dpt = total.div(ticks);
             }
         }
+        return dpt;
    }
 }
+
+
 
 
 
@@ -83,26 +84,26 @@ impl Transition {
 mod tests {
     use crate::ui::transtions::{Transition, TransitionType};
 
+    //Test is designed to create a transition and push it through a transition phase
+    //If the transition phase is caught by the check end. Pass 
     #[test]
-    fn get_dist_per_tick() {
-        let mut t = Transition::new(
+    fn test_inc_pos() {
+        let mut t_1 = Transition::new(
             TransitionType::Slide,
-            glam::Vec2::new(2.0,2.0),
-            glam::Vec2::new(122.0,122.0),
+            glam::Vec2::new(0.0,0.0),
+            glam::Vec2::new(10.0,10.0),
         );
-        let answer = glam::Vec2::new(2.0,2.0);
-        assert_eq!(true , true);
-    }
-
-    #[test]
-    fn test_normlize_vector() {
-        let mut t = Transition::new(
+        while t_1.arrived == false {
+            t_1.inc_transition();
+        }
+        let mut t_2 = Transition::new(
             TransitionType::Slide,
-            glam::Vec2::new(2.0,2.0),
-            glam::Vec2::new(122.0,122.0),
+            glam::Vec2::new(10.0,10.0),
+            glam::Vec2::new(0.0,0.0),
         );
-        t.arived_at_dest();
-        assert!(true);
+        while t_2.arrived == false {
+            t_2.inc_transition();
+        }
+        assert!(t_1.arrived && t_2.arrived);
     }
-
 }
