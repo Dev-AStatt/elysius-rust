eb52178fd34237adc5bc172b487a779e3e09a006
AStatt-Desktop <AStatt@Protonmail.com>
2022-07-02 14:03:23 -0700

    Built Mouse struct into a OOP object instead of a mutable struct

--- src/main_state/io.rs
+++ src/main_state/io.rs
@@ -1,19 +1,56 @@
+use ggez::graphics;
 
-#[derive(PartialEq)]
+
+#[derive(PartialEq, Copy, Clone)]
 pub enum MouseFocus {
     Background,
     Body(usize),
     Menu,
 }
 
-pub struct MouseState {
-    pub focus: MouseFocus,
-    pub pos: (f32, f32),
-    pub click_down: bool,
+pub struct Mouse {
+    focus: MouseFocus,
+    pos: (f32, f32),
+    click_down: bool,
+}
+//Getters and Setters
+impl Mouse {
+    pub fn set_focus(self: &mut Self, f: MouseFocus) {self.focus = f;}
+    pub fn set_pos_f32(self: &mut Self, p: (f32,f32)) {self.pos = p;}
+    pub fn set_pos_vec2(self: &mut Self, p: glam::Vec2) {self.pos = (p.x,p.y);}
+    pub fn set_click_down(self: &mut Self, b: bool) {self.click_down = b;}
+
+    pub fn get_click_down(&self) -> bool {return self.click_down;}
+    pub fn get_pos_f32(&self) -> (f32,f32) {return self.pos;}
+    pub fn get_pos_vec2(&self) -> glam::Vec2 {return glam::Vec2::new(self.pos.0,self.pos.1);}
+    pub fn get_focus(&self) -> MouseFocus {return self.focus;}
 }
 
 
-// 0---------------------INPUT EVENTS------------------------------------------0
+impl Mouse {
+    pub fn new() -> Self {
+        Mouse { 
+            focus: MouseFocus::Background,
+            pos: (0.0,0.0), 
+            click_down: false 
+        }
+    }
+    pub fn get_focus_as_string(&self) -> String {
+        let mut str = String::from("Mouse Focus: ");
+        match self.focus {
+            MouseFocus::Background => {
+                str.push_str("Background");
+            }
+            MouseFocus::Body(id) => {
+                str.push_str(&("Entity ".to_owned()+ &id.to_string()));
+            }
+            MouseFocus::Menu => {
+                str.push_str("Menu");
+            }
+        }
+        return str;
+    }
 
+}
 
 
