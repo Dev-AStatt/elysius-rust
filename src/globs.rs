0e28ffa81f4651b8774453ebb6bebb05e81dd0b8
AStatt-Desktop <AStatt@Protonmail.com>
2022-07-03 16:53:56 -0700

    changed globs from f32 to glam::Vec2

--- src/globs.rs
+++ src/globs.rs
@@ -1,5 +1,7 @@
+use glam;
 //GLOBAL VALUE for screen size
-pub const SCREEN_SIZE: (f32, f32) = (1024.0 ,1024.0);
-pub const SCREEN_OFFSET: (f32, f32) = (512.0, 512.0);
+
+pub const SCREEN_SIZE: glam::Vec2 = glam::Vec2::new(1024.0 ,1024.0);
+pub const SCREEN_OFFSET: glam::Vec2 = glam::Vec2::new(512.0, 512.0);
 //Names for Planets
 
