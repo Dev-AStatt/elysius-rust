c6ced3debbde1ae6cd8701717ac4edc36c261443
AStatt-Desktop <AStatt@Protonmail.com>
2022-07-08 11:36:24 -0700

    added mouse over button function

--- src/ui/ui_tools/button.rs
+++ src/ui/ui_tools/button.rs
@@ -2,23 +2,29 @@ use ggez::{
     graphics::{self,Color},
     Context,
 };
+use crate::utilities;
+
 use super::{color_palette, disp_item::BoxSize};
 use super::disp_item;
 #[derive(Clone)]
 pub struct Button {
     dp: disp_item::DisplayItem,
-    clicked: bool,
+    mouse_over: bool,
     col_focus: Color,
     col_unfocus: Color,
 
 }
 
 impl Button {
-    pub fn is_clicked(&self) -> bool {return self.clicked;}
+    pub fn mouse_over(&self) -> bool {return self.mouse_over;}
     pub fn hight(&self) -> f32 {return self.dp.hight();} 
     pub fn width(&self) -> f32 {return self.dp.width();} 
+    pub fn size(&self) -> glam::Vec2 {return self.dp.box_size().size(); }
     pub fn col_focus(&self) -> Color {return self.col_focus}
     pub fn col_unfocus(&self) -> Color {return self.col_unfocus}
+    pub fn rel_pos(&self) -> glam::Vec2 {return self.dp.rel_pos()}
+    //Sets
+    pub fn set_mouse_over(self: &mut Self) {self.mouse_over = true;}
 
     pub fn new(
         size_type: BoxSize,
@@ -39,7 +45,7 @@ impl Button {
 
         Button {
             dp,
-            clicked: false,
+            mouse_over: false,
             col_focus: col.color_4, 
             col_unfocus: col.color_5,
         }
@@ -48,6 +54,22 @@ impl Button {
     pub fn draw(&self, canvas: &mut graphics::Canvas, menu_pos: glam::Vec2) {
         self.dp.draw(canvas, menu_pos);
     }
+
+    pub fn mouse_over_bttn(&self, menu_pos: glam::Vec2, mouse_pos: glam::Vec2
+    ) -> bool {
+        let tl = menu_pos + self.rel_pos();
+        let br = tl + self.size(); 
+        if utilities::point_in_square(tl, br, mouse_pos) {return true;}
+        else {return false;} 
+    }
+
+    pub fn update(self: &mut Self, menu_pos: glam::Vec2, mouse_pos: glam::Vec2) {
+        if self.mouse_over_bttn(menu_pos,mouse_pos) {self.mouse_over = true;}
+        else {self.mouse_over = false;}
+    }
+
+
+
 }
 
 
