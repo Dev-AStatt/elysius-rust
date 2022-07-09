30d157af15e75002e7924f0e913bcfb7003ca191
AStatt-Desktop <AStatt@Protonmail.com>
2022-07-09 16:55:58 -0700

    re-write of the set mouse focus function
    this makes it much easier to read and adds comments

--- src/main_state/loop_update.rs
+++ src/main_state/loop_update.rs
@@ -26,31 +26,24 @@ impl ElysiusMainState {
         self.mouse.set_focus(io::MouseFocus::Background);
 
         for i in 0..self.entities_id.len() {
-            let sprite_offset_scaled = self.entities.draw_comp[i].sprite_offset() * self.state.scale();
-            let adj_pos_for_input = (
-                self.entities.draw_comp[i].screen_pos().x + sprite_offset_scaled.x, 
-                self.entities.draw_comp[i].screen_pos().y + sprite_offset_scaled.y
-            );
-
-            if utilities::point_in_circle(&self.mouse.get_pos_f32(),
-                adj_pos_for_input, 
-            self.entities.draw_comp[i].sprite_offset().x * self.state.scale().x,
-            ) {
+            if self.mouse_over_ent(i) {
                 self.mouse.set_focus(io::MouseFocus::Body(i));
             }
         }    
     }
 
+    //Function will take in an entity id and return if the mouse 
+    //position is inside the entity position on the screen
     fn mouse_over_ent(&self, ent: usize) -> bool {
         let sprite_offset_scaled = self.entities.draw_comp[ent].sprite_offset() * self.state.scale();
-        let adj_pos_for_input = (
-            self.entities.draw_comp[ent].screen_pos().x + sprite_offset_scaled.x, 
-            self.entities.draw_comp[ent].screen_pos().y + sprite_offset_scaled.y
-        );
+        //This is the screen position of the entity adjusted for the scale and position on screen
+        let final_pos =    self.entities.draw_comp[ent].screen_pos() + sprite_offset_scaled;
 
-        if utilities::point_in_circle(&self.mouse.get_pos_f32(),
-            adj_pos_for_input, 
-        self.entities.draw_comp[ent].sprite_offset().x * self.state.scale().x,
+        let new_rad = self.entities.draw_comp[ent].sprite_offset().x * self.state.scale().x;
+        if utilities::point_in_circle_vec2(
+            &self.mouse.get_pos_vec2(),
+            final_pos,
+            new_rad
         ) {return true;}
         else {return false;}
     }
