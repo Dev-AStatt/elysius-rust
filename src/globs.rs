81e8ba198ffbf7b7ccfd20e30c215c893ef68b15
AStatt-Desktop <AStatt@Protonmail.com>
2022-07-15 12:17:41 -0700

    add gravitational constant to game

--- src/globs.rs
+++ src/globs.rs
@@ -1,7 +1,7 @@
 //GLOBAL VALUE for screen size
 
 pub const SCREEN_SIZE: (f32,f32) = (1024.0 ,1024.0);
-//pub const SCREEN_OFFSET: (f32,f32) = (512.0, 512.0);
+pub const GRAV_CONST: f64 = (6.6_f64 * 10.0_f64).powi(-11);
 
 //Names for Planets
 
