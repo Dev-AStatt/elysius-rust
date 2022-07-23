40fddff3c657f59e2fd9e3aab71f1f50e80dc2d6
AStatt-Desktop <AStatt@Protonmail.com>
2022-07-22 17:28:03 -0700

    Organization changes

--- src/ecs/pos_comp.rs
+++ src/ecs/pos_comp.rs
@@ -24,31 +24,7 @@ impl PosComponent {
 
         }
     }
-
-    pub fn solar_pos(&self) -> glam::Vec2 {return self.solar_pos;}
-    pub fn solar_system(&self) -> i32 {return self.solar_system;}
-    pub fn in_transfer(&self) -> bool {return self.in_transfer;}
-    pub fn screen_pos(&self) -> glam::Vec2 {return self.screen_pos;}
-    pub fn tails(&self) -> Vec<Tail> {return self.tails.clone();}
-
-    pub fn set_solar_pos(self: &mut Self, pos: glam::Vec2) {self.solar_pos = pos;}
-    pub fn set_in_transfer(self: &mut Self, b: bool) {self.in_transfer = b;}
-    //Inc solar pos will add the current position to history, then increase postiion. 
-    pub fn inc_solar_pos(self: &mut Self, inc: glam::Vec2) {
-        self.solar_pos += inc;
-    }
-    //Inc x will just call inc_solar_pos with {inc, 0.0}
-    pub fn inc_solar_pos_x(self: &mut Self, inc: f32) {self.inc_solar_pos(glam::Vec2::new(inc,0.0));}
-    //Inc y will just call inc_solar_pos with {0.0, inc}
-    pub fn inc_solar_pos_y(self: &mut Self, inc: f32) {self.inc_solar_pos(glam::Vec2::new(0.0,inc));}
-    pub fn set_screen_pos(self: &mut Self, pos: glam::Vec2) {self.screen_pos = pos;}
-
-    
-   pub fn is_in_system(&self, system: i32) -> bool {
-        if self.solar_system == system {return true;}
-        else {return false;}
-    }
-   
+ 
     pub fn update(
         self: &mut Self,
         state: &game_state::GameState,
@@ -56,10 +32,9 @@ impl PosComponent {
         orb_center_pos: Option<glam::Vec2>,
         orbit_id: usize,
     ) {
-        if let Some(pos) = orb_center_pos {
-           self.add_to_history(pos, orbit_id);
-        }
-       self.set_screen_pos(
+        self.update_tails(orb_center_pos,orbit_id);        
+
+        self.set_screen_pos(
             self.get_orbit_final_pos(
                 state.scale(), 
                 state.player_screen_offset_pos(), 
@@ -67,7 +42,39 @@ impl PosComponent {
             )
         ) 
     }
-    fn add_to_history(self: &mut Self, orb_center_pos: glam::Vec2, orbit_id: usize) {
+    fn update_tails(self: &mut Self, orb_center_pos: Option<glam::Vec2>, orbit_id: usize) {
+        //Add to tails if we should
+        if let Some(pos) = orb_center_pos {
+            self.add_tail(pos, orbit_id);
+        }
+        //Update Tails we have 
+        self.tails.iter_mut().for_each(|t| {
+            t.update();
+        });  
+        //remove tails we dont need
+        self.tails.retain(|t| !t.expired());
+    }
+
+
+   pub fn tails(&self) -> Vec<Tail> {
+        if self.in_transfer {
+            let mut alt_tails = self.tails.clone();
+            alt_tails.iter_mut().for_each(|t| {
+                t.set_hilight();
+            });
+            return alt_tails;
+        } else {
+            return self.tails.clone();
+        }
+    }
+
+   pub fn is_in_system(&self, system: i32) -> bool {
+        if self.solar_system == system {return true;}
+        else {return false;}
+    }
+      
+
+    fn add_tail(self: &mut Self, orb_center_pos: glam::Vec2, orbit_id: usize) {
         self.history_count += 1;
         if self.history_count > 99 {
             let new_tail = Tail::new(
@@ -89,7 +96,21 @@ impl PosComponent {
         let disp_adj = sprite_offset * scale;
         return sprite_pos - disp_adj + player_offset; 
     }
-}
- 
 
+    pub fn solar_pos(&self) -> glam::Vec2 {return self.solar_pos;}
+    pub fn solar_system(&self) -> i32 {return self.solar_system;}
+    pub fn in_transfer(&self) -> bool {return self.in_transfer;}
+    pub fn screen_pos(&self) -> glam::Vec2 {return self.screen_pos;}
+
+    pub fn set_solar_pos(self: &mut Self, pos: glam::Vec2) {self.solar_pos = pos;}
+    pub fn set_in_transfer(self: &mut Self, b: bool) {self.in_transfer = b;}
+    //Inc solar pos will add the current position to history, then increase postiion. 
+    pub fn inc_solar_pos(self: &mut Self, inc: glam::Vec2) {self.solar_pos += inc;}
+    //Inc x will just call inc_solar_pos with {inc, 0.0}
+    pub fn inc_solar_pos_x(self: &mut Self, inc: f32) {self.inc_solar_pos(glam::Vec2::new(inc,0.0));}
+    //Inc y will just call inc_solar_pos with {0.0, inc}
+    pub fn inc_solar_pos_y(self: &mut Self, inc: f32) {self.inc_solar_pos(glam::Vec2::new(0.0,inc));}
+    pub fn set_screen_pos(self: &mut Self, pos: glam::Vec2) {self.screen_pos = pos }
+ 
+}
 
